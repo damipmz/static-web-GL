a7985437510d7a20d1627ee26a0683560f9f7786
Damián Ponce Martínez <damianponce97@gmail.com>
2023-05-25 01:20:38 -0300

    style buttons

--- scripts/gameQuestions.js
+++ scripts/gameQuestions.js
@@ -1,12 +1,12 @@
-window.addEventListener('DOMContentLoaded', () => {
+window.addEventListener("DOMContentLoaded", () => {
 
-    const question = document.getElementById('question')
-    const answer1 = document.getElementById('answer-1-question')
-    const answer2 = document.getElementById('answer-2-question')
-    const answer3 = document.getElementById('answer-3-question')
+    const question = document.getElementById("question");
+    const answer1 = document.getElementById("answer-1-question");
+    const answer2 = document.getElementById("answer-2-question");
+    const answer3 = document.getElementById("answer-3-question");
 
+    /* ¿Cuál de los siguientes animes presenta a los personajes Monkey D.
+      Luffy, Roronoa Zoro y Nami en su tripulación? */
 
-
-})
-
-
+    
+});
