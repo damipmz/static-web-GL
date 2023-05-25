de9ff38d6cc9bb3ad55a1c9ed208d5256d809b9e
lautaro ruciani <lautaroruciani@gmail.com>
2023-05-25 01:41:44 -0300

    change style css

---
+++ scripts/gameImages.js
@@ -0,0 +1,21 @@
+const questions = [
+    {
+    id:1,
+    image:'/questionimage.jpg',
+    answers:{
+            answer1:{
+                answerAnime:'Naruto',
+                answerValor:false
+            },
+            answer2:{
+                answerAnime:'One piece',
+                answerValor:true
+            },
+            answer3:{
+                answerAnime:'Bleach',
+                answerValor:false
+            }
+        }
+    },
+    
+]
