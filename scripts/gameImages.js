bdf6063d928d0d3f81d33de7b1ba1b3f8c515f95
lautaro ruciani <lautaroruciani@gmail.com>
2023-05-27 21:45:25 -0300

    add javascript functions in game Images

--- scripts/gameImages.js
+++ scripts/gameImages.js
@@ -4,40 +4,42 @@ const questions = [
     image: "https://pbs.twimg.com/media/CuqyqeyXYAAjuRe.jpg",
     answers: {
       answer1: {
-        answerAnime: "Santiago",
-        answerValor: false,
-      },
-      answer2: {
-        answerAnime: "Gabi",
+        answerAnime: "One Piece",
         answerValor: true,
       },
+      answer2: {
+        answerAnime: "El increible castillo vagabundo",
+        answerValor: false,
+      },
       answer3: {
-        answerAnime: "XD",
+        answerAnime: "Bleach",
         answerValor: false,
       },
     },
   },
   {
     id: 2,
-    image: "https://laverdadnoticias.com/__export/1606238843594/sites/laverdad/img/2020/11/24/legion_de_reconocimiento_shingeki_no_kyojin.png_819446995.png",
+    image:
+      "https://laverdadnoticias.com/export/sites/laverdad/img/2020/09/25/10_cosas_que_no_sabxas_sobre_las_espadas_nichirin_6.jpg_1118837850.jpg",
     answers: {
       answer1: {
-        answerAnime: "Martin",
-        answerValor: false,
-      },
-      answer2: {
-        answerAnime: "Bleach",
+        answerAnime: "Kimtetsu No Yaiba",
         answerValor: true,
       },
+      answer2: {
+        answerAnime: "Sword Art Online",
+        answerValor: false,
+      },
       answer3: {
-        answerAnime: "Atack on mario",
+        answerAnime: "Naruto",
         answerValor: false,
       },
     },
   },
   {
     id: 3,
-    image: "https://laverdadnoticias.com/__export/1606238843594/sites/laverdad/img/2020/11/24/legion_de_reconocimiento_shingeki_no_kyojin.png_819446995.png",
+    image:
+      "https://laverdadnoticias.com/__export/1606238843594/sites/laverdad/img/2020/11/24/legion_de_reconocimiento_shingeki_no_kyojin.png_819446995.png",
     answers: {
       answer1: {
         answerAnime: "Martin",
@@ -55,7 +57,8 @@ const questions = [
   },
   {
     id: 4,
-    image: "https://laverdadnoticias.com/__export/1606238843594/sites/laverdad/img/2020/11/24/legion_de_reconocimiento_shingeki_no_kyojin.png_819446995.png",
+    image:
+      "https://laverdadnoticias.com/__export/1606238843594/sites/laverdad/img/2020/11/24/legion_de_reconocimiento_shingeki_no_kyojin.png_819446995.png",
     answers: {
       answer1: {
         answerAnime: "Martin",
@@ -75,6 +78,7 @@ const questions = [
 
 const questionImage = document.getElementById("question-image");
 const numberQuestion = document.getElementById("numberQuestion-image");
+const question = document.getElementById("question");
 
 const allButtonsImage = document.querySelectorAll("button");
 
@@ -83,49 +87,82 @@ const allButtonsImage = document.querySelectorAll("button");
 /** Creo unas varialbes auxiliares para que no se mezclen con las de los otros botones... */
 let questionInitiator = 0;
 let answerInitiator = 1;
-questionImage.setAttribute('src', questions[questionInitiator].image);
-allButtonsImage.forEach((button) => {
 
-    button.textContent = questions[questionInitiator].answers[`answer${answerInitiator}`].answerAnime;
-    answerInitiator += 1;
+DELAY_MS_PAGE = 500
+questionImage.setAttribute("src", questions[questionInitiator].image);
+allButtonsImage.forEach((button) => {
+  button.textContent =
+    questions[questionInitiator].answers[
+      `answer${answerInitiator}`
+    ].answerAnime;
+  answerInitiator += 1;
 
   /** Iterador Question para saber en que pregunta estamos y asi poder recorrer el array */
   let questionIterator = 1;
-  
+
   /** Iterador de respuesta, para luego cambiar el contenido de las preguntas */
   let answerIterator = 1;
-  
+
   /** Contador para saber cuantas acerto */
   let points = 0;
-  
+
   button.addEventListener("click", (event) => {
+    //Animacion para que le de tiempo a cargar a la imagen
+    allButtonsImage.forEach((button) => {
+      button.classList.add("disappear");
+      question.classList.add("disappear");
+      button.classList.remove("appear");
+      question.classList.remove("appear");
+    });
+
+    setTimeout(() => {
+      allButtonsImage.forEach((button) => {
+        button.classList.remove("disappear");
+        question.classList.remove("disappear");
+        button.classList.add("appear");
+        question.classList.add("appear");
+      });
+    }, DELAY_MS_PAGE);
 
     /** Cada vez que se registre un click, cambiara que pregunta se encuentra, recorro de nuevo los botones para reasignarle el contenido
      * de las respuestas.
      */
-    numberQuestion.textContent = `Pregunta ${questions[questionIterator].id}`;
-    allButtonsImage.forEach((buttonCont) => {
-      questionImage.setAttribute('src', questions[questionIterator].image);
-      buttonCont.textContent = questions[questionIterator].answers[`answer${answerIterator}`].answerAnime;
-      answerIterator += 1;
-
-    });
-    answerIterator = 1;
-    for(let iterator = 1 ; iterator < 4 ; iterator ++){
-        if((questions[questionIterator].answers[`answer${iterator}`].answerAnime == event.target.textContent) && (questions[questionIterator].answers[`answer${iterator}`].answerValor == true)){
-            console.log('respuesta correcta +1 punto')
-            points+=1
-            console.log(`total de puntos: ${points}`)
+    if (questionIterator == 4) {
+      allButtonsImage.forEach((button) => {
+        button.classList.add("disappear");
+        question.classList.add("disappear");
+        button.classList.remove("appear");
+        question.classList.remove("appear");
+      });
+      setTimeout(() => {
+        window.location.assign("./pointsGameQuestions.html");
+      }, DELAY_MS_PAGE);
+      
+    } else {
+      numberQuestion.textContent = `Pregunta ${questions[questionIterator].id}`;
+      allButtonsImage.forEach((buttonCont) => {
+        questionImage.setAttribute("src", questions[questionIterator].image);
+        buttonCont.textContent =
+          questions[questionIterator].answers[
+            `answer${answerIterator}`
+          ].answerAnime;
+        answerIterator += 1;
+      });
+      answerIterator = 1;
+      for (let iterator = 1; iterator < 4; iterator++) {
+        if (questions[questionIterator].answers[`answer${iterator}`].answerAnime == event.target.textContent &&questions[questionIterator].answers[`answer${iterator}`].answerValor == true) 
+        {
+          console.log("respuesta correcta +1 punto");
+          points += 1;
+          console.log(`total de puntos: ${points}`);
         }
-    }   
-    
+      }
+      console.log("hiciste un click");
+      console.log(questionIterator);
+    }
+    console.log(questionIterator+'Este es el numero de pregunta del ciclo')
     questionIterator = questionIterator + 1;
-    console.log('hiciste un click')
-
   });
 });
 
-/**  setTimeout(() => {
-    console.log("hiciste click en un boton");
-}, 300);
-*/
+
