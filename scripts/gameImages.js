9ff7a04eb4eb6d4b98ba9d8a9ffdd6ac96b389a0
lautaro ruciani <lautaroruciani@gmail.com>
2023-05-28 00:41:56 -0300

    add temporal changes

--- scripts/gameImages.js
+++ scripts/gameImages.js
@@ -24,11 +24,11 @@ const questions = [
     answers: {
       answer1: {
         answerAnime: "Kimtetsu No Yaiba",
-        answerValor: true,
+        answerValor: false,
       },
       answer2: {
         answerAnime: "Sword Art Online",
-        answerValor: false,
+        answerValor: true,
       },
       answer3: {
         answerAnime: "Naruto",
@@ -39,18 +39,18 @@ const questions = [
   {
     id: 3,
     image:
-      "https://laverdadnoticias.com/__export/1606238843594/sites/laverdad/img/2020/11/24/legion_de_reconocimiento_shingeki_no_kyojin.png_819446995.png",
+      "https://i.pinimg.com/originals/56/d5/00/56d500c8adb9b09e5c0e25cefa25b70c.jpg",
     answers: {
       answer1: {
-        answerAnime: "Martin",
+        answerAnime: "Serial Experiments Lain",
         answerValor: false,
       },
       answer2: {
-        answerAnime: "Bleach",
+        answerAnime: "Neon Genesis Evangelion",
         answerValor: true,
       },
       answer3: {
-        answerAnime: "Atack on mario",
+        answerAnime: "Devilman Crybaby",
         answerValor: false,
       },
     },
@@ -82,14 +82,29 @@ const question = document.getElementById("question");
 
 const allButtonsImage = document.querySelectorAll("button");
 
-/** Con un foreach a todos los nodos les agrego la misma funcionalidad, para no escribir codigo de mas */
+
 
 /** Creo unas varialbes auxiliares para que no se mezclen con las de los otros botones... */
 let questionInitiator = 0;
+
 let answerInitiator = 1;
 
-DELAY_MS_PAGE = 500
+
+/** Iterador Question para saber en que pregunta estamos y asi poder recorrer el array */
+let questionIterator = 1;
+
+/** Iterador de respuesta, para luego cambiar el contenido de las preguntas */
+let answerIterator = 1;
+
+/** Contador para saber cuantas acerto */
+let points = 0;
+
+
+DELAY_MS_PAGE = 500;
 questionImage.setAttribute("src", questions[questionInitiator].image);
+
+/** Con un foreach a todos los nodos les agrego la misma funcionalidad, para no escribir codigo de mas */
+
 allButtonsImage.forEach((button) => {
   button.textContent =
     questions[questionInitiator].answers[
@@ -97,16 +112,12 @@ allButtonsImage.forEach((button) => {
     ].answerAnime;
   answerInitiator += 1;
 
-  /** Iterador Question para saber en que pregunta estamos y asi poder recorrer el array */
-  let questionIterator = 1;
-
-  /** Iterador de respuesta, para luego cambiar el contenido de las preguntas */
-  let answerIterator = 1;
-
-  /** Contador para saber cuantas acerto */
-  let points = 0;
-
   button.addEventListener("click", (event) => {
+    questionIterator = questionIterator + 1;
+    
+    //questionIterator = questionIterator + 1;
+
+    console.log("Question iterator:", questionIterator);
     //Animacion para que le de tiempo a cargar a la imagen
     allButtonsImage.forEach((button) => {
       button.classList.add("disappear");
@@ -127,17 +138,17 @@ allButtonsImage.forEach((button) => {
     /** Cada vez que se registre un click, cambiara que pregunta se encuentra, recorro de nuevo los botones para reasignarle el contenido
      * de las respuestas.
      */
-    if (questionIterator == 4) {
+
+    if (questionIterator === 4) {
       allButtonsImage.forEach((button) => {
-        button.classList.add("disappear");
-        question.classList.add("disappear");
         button.classList.remove("appear");
         question.classList.remove("appear");
+        button.classList.add("disappear");
+        question.classList.add("disappear");
       });
       setTimeout(() => {
         window.location.assign("./pointsGameQuestions.html");
       }, DELAY_MS_PAGE);
-      
     } else {
       numberQuestion.textContent = `Pregunta ${questions[questionIterator].id}`;
       allButtonsImage.forEach((buttonCont) => {
@@ -150,19 +161,22 @@ allButtonsImage.forEach((button) => {
       });
       answerIterator = 1;
       for (let iterator = 1; iterator < 4; iterator++) {
-        if (questions[questionIterator].answers[`answer${iterator}`].answerAnime == event.target.textContent &&questions[questionIterator].answers[`answer${iterator}`].answerValor == true) 
-        {
+        if (
+          questions[questionIterator].answers[`answer${iterator}`]
+            .answerAnime === event.target.textContent &&
+          questions[questionIterator].answers[`answer${iterator}`]
+            .answerValor === true
+        ) {
           console.log("respuesta correcta +1 punto");
           points += 1;
-          console.log(`total de puntos: ${points}`);
         }
+       // debugger
       }
-      console.log("hiciste un click");
-      console.log(questionIterator);
     }
-    console.log(questionIterator+'Este es el numero de pregunta del ciclo')
-    questionIterator = questionIterator + 1;
+    
+    console.log("hiciste un click");
+    console.log("total de puntos", points);
+    console.log('-----')
+    
   });
 });
-
-
